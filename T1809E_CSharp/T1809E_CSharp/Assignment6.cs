0120aa5a5f573c0060873a5f7443567d952c5cca
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-08-05 21:06:44 +0700

    assignment 6 + lock sync

--- T1809E_CSharp/T1809E_CSharp/Assignment6.cs
+++ T1809E_CSharp/T1809E_CSharp/Assignment6.cs
@@ -10,34 +10,102 @@ namespace T1809E_CSharp
         {
             Console.WriteLine("Input n=");
             int n = Convert.ToInt32(Console.ReadLine());
+            Timer timer = new Timer();
             Thread t1 = new Thread(delegate()
             {
-                for (int i = 0; true; i++)
+                for (; ;)
                 {
-                    for (int j = 0; j < 60; j++)
+                    lock (timer)
                     {
-                        Console.WriteLine(i.ToString("D2")+
-                                          ":"+j.ToString("D2"));
-                        Thread.Sleep(1000);
+                        timer.sec = 0;
+                        for (; timer.sec < 60; timer.sec++)
+                        {
+                            Console.WriteLine(timer.min.ToString("D2")
+                                              +":"+ timer.sec.ToString("D2"));
+                            Thread.Sleep(1000);
+                        }
+
+                        timer.min++;
                     }
                 }
+                
             });
-            
-            Thread t2 = new Thread(delegate()
+            Thread t11 = new Thread(delegate()
             {
-                int x1 = 1, x2 = 1, x3 = 2;
-                for (int i = 0; x1+x2 < n; i++)
+                while (true)
                 {
-                    x3 = x1 + x2;
-                    x1 = x2;
-                    x2 = x3;
-                    Console.WriteLine("Fibo: " + x3);
-                    Thread.Sleep(500);
+                    lock (timer)
+                    {
+                        timer.sec = 0;
+                        for (; timer.sec < 60; timer.sec++)
+                        {
+                            Console.WriteLine(timer.min.ToString("D2")
+                                              +":"+ timer.sec.ToString("D2"));
+                            Thread.Sleep(1000);
+                        }
+
+                        timer.min++;
+                    }
                 }
+                
             });
             t1.IsBackground = true;
+            t11.IsBackground = true;
+            
+            Fibo f = new Fibo();
+            Thread t2 = new Thread(delegate()
+            {
+                for (; ; )
+                {
+                    lock (f)
+                    {
+                        if (f.x1 + f.x2 > n)
+                        {
+                            break;
+                        }
+                        f.x3 = f.x1 + f.x2;
+                        f.x1 = f.x2;
+                        f.x2 = f.x3;
+                        Console.WriteLine("t2 - Fibo: " + f.x3);
+                        Thread.Sleep(500);
+                    } 
+                }
+            });
+            Thread t22 = new Thread(delegate()
+            {
+                for (; ; )
+                {
+                    lock (f)
+                    {
+                        if (f.x1 + f.x2 > n)
+                        {
+                            break;
+                        }
+                        f.x3 = f.x1 + f.x2;
+                        f.x1 = f.x2;
+                        f.x2 = f.x3;
+                        Console.WriteLine("t22 - Fibo: " + f.x3);
+                        Thread.Sleep(500);
+                    } 
+                }
+            });
             t1.Start();
+            t11.Start();
             t2.Start();
+            t22.Start();
         }
+    }
+
+    public class Fibo
+    {
+        public int x1 = 1;
+        public int x2 = 1;
+        public int x3 = 2;
+    }
+
+    public class Timer
+    {
+        public int min = 0;
+        public int sec = 0;
     }
 }
---
+++ T1809E_CSharp/T1809E_CSharp/Session7.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+
+namespace T1809E_CSharp
+{
+    public class Session7
+    {
+        public static void Main(string[] args)
+        {
+            Numberic n = new Numberic();
+            Thread t1 = new Thread(delegate() {
+                for (int i = 0; i < 100; i++)
+                {
+                    lock (n)
+                    {
+                        Console.WriteLine("t1 - "+n.age);
+                        n.age++; 
+                    }
+                    Thread.Sleep(300);
+                }
+            });
+            
+            Thread t2 = new Thread(delegate() {
+                for (int i = 0; i < 100; i++)
+                {
+                    lock (n)
+                    {
+                        Console.WriteLine("t2 - "+n.age);
+                        n.age++;
+                    }
+                    Thread.Sleep(300);
+                }
+            });
+            t1.Start();
+           // t1.Join();
+            t2.Start();
+        }
+    }
+
+    public class Numberic
+    {
+        public int age = 0;
+    }
+}
